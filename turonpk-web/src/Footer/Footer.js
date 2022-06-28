dfc873baab77825362cb2753bb290873e3f998e8
Mehgillani <70068079+MehmoodGillani@users.noreply.github.com>
2022-06-28 23:30:47 +0500

    Update Footer.js

--- turonpk-web/src/Footer/Footer.js
+++ turonpk-web/src/Footer/Footer.js
@@ -20,18 +20,16 @@ export const Footer = () => {
                 <div class="footer-center">
                     <div>
                         <i class="fa fa-map-marker"></i>
-                        <p><span>309 - Rupa Solitaire,
-                            Bldg. No. A - 1, Sector - 1</span>
-                            Mahape, Navi Mumbai - 400710</p>
+                        <p><span>abc, karachi, pakistan</p>
                     </div>
 
                     <div>
                         <i class="fa fa-phone"></i>
-                        <p>+91 22-27782183</p>
+                        <p>+92 3433333333</p>
                     </div>
                     <div>
                         <i class="fa fa-envelope"></i>
-                        <p><a href="mailto:support@eduonix.com">support@Turonpk.com</a></p>
+                        <p><a href="mailto:#">support@Turonpk.com</a></p>
                     </div>
                 </div>
                 <div class="footer-right">
